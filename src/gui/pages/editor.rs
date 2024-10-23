bb138631c8437c1d06b642a75dee03865dbe0e8a
ouadev <ouadev@users.noreply.github.com>
2024-10-23 11:16:29 +0200

    editor status bar: show id of the current entry

--- src/gui/pages/editor.rs
+++ src/gui/pages/editor.rs
@@ -193,7 +193,7 @@ impl EditorPage {
             horizontal_space(),
             text(format!(
                 "{}",
-                &self.entry_by_id(self.curr_entry_id).unwrap().type_entry
+                &self.entry_by_id(self.curr_entry_id).unwrap().id
             ))
             .size(style_constants::STYLE_TEXT_SIZE_BUTTON)
         ])
