75ee96bc6dfe4804aba27341d39d98a43ec36196
ouadev <ouadev@users.noreply.github.com>
2024-11-21 01:31:12 +0100

    separate file for palette - dark mode as default.

--- src/gui/styles/mod.rs
+++ src/gui/styles/mod.rs
@@ -1,3 +1,4 @@
 pub mod button;
+pub mod palette;
 pub mod style_conf;
 pub mod text_editor;
--- src/gui/styles/style_conf.rs
+++ src/gui/styles/style_conf.rs
@@ -1,49 +1,12 @@
-use iced::color;
+use super::palette::Palette;
 use iced::font::{Family, Stretch, Style, Weight};
-use iced::{Color, Font};
+use iced::Font;
 
-//
-pub struct Palette {
-    pub background_main: Color,
-    pub background_secondary: Color,
-    pub control_main: Color,
-    pub control_secondary: Color,
-    pub control_disabled: Color,
-    pub text: Color,
-    // ...
-}
-
-impl Palette {
-    pub const LIGHT: Self = Self {
-        background_main: color!(0xffffff),
-        background_secondary: color!(0xd9d9d9),
-        control_main: color!(0xb6deca),
-        control_secondary: color!(0xb6deca),
-        control_disabled: color!(0x000000),
-        text: color!(0x000000),
-    };
-
-    pub const LIGHT_CATPPUCCIN: Self = Self {
-        background_main: color!(0xeff1f5),
-        background_secondary: color!(0xdce0e8),
-        control_main: color!(0x04a5e5),
-        control_secondary: color!(0xb6deca),
-        control_disabled: color!(0x000000),
-        text: color!(0x4c4f69),
-    };
-
-    pub const DARK: Self = Self {
-        background_main: color!(0x282A36),
-        background_secondary: color!(0x44475a),
-        control_main: color!(0x6272a4),
-        control_secondary: color!(0xb6deca),
-        control_disabled: color!(0x000000),
-        text: color!(0xFFFFFF),
-    };
-}
-
+/**
+* Return the current Palette of colors to paint the UI.
+*/
 pub fn palette() -> Palette {
-    Palette::LIGHT_CATPPUCCIN
+    Palette::DARK
 }
 // Font families for different use cases.
 pub const FONT_FAMILY_DEFAULT: Family = Family::SansSerif;
