ab942650ac3d879f139f20102d92a4f46f09fdfb
ouadev <ouadev@users.noreply.github.com>
2025-04-07 12:10:41 +0200

    package-win.sh: fix directory creation test case

--- package-win.sh
+++ package-win.sh
@@ -5,8 +5,9 @@
 # . use rustup to add windows target files. [missing command]
 # . Install build tools : brew install mingw-w64
 # . Build : cargo build --target x86_64-pc-windows-gnu --profile release --bin gui
-VERSION="0.0.12"
+VERSION="0.9.2-05801a"
 
+mkdir -p packages/windows-release
 # Build Windows Resource file Object.
 x86_64-w64-mingw32-windres resources/windows/resources.rc -O coff -o packages/windows-release/tirra-win.lib
 
