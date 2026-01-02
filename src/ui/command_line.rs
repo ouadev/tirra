b974ac9376ea2cc9a84f5699c26561f07511d06b
ouadev <ouadev@users.noreply.github.com>
2026-01-02 23:11:37 +0100

    CLI pwd: use for all commands

--- src/ui/command_line.rs
+++ src/ui/command_line.rs
@@ -14,11 +14,11 @@ Tirra Command Line.
 
 Usage:
 tirra-cli ver
-tirra-cli add     DB_FILE PWD_FILE DATE
-tirra-cli delete  DB_FILE PWD_FILE ID
-tirra-cli stat    DB_FILE PWD_FILE
-tirra-cli decrypt DB_FILE PWD_FILE
-tirra-cli encrypt PL_FILE PWD_FILE 
+tirra-cli add     DB_FILE  DATE
+tirra-cli delete  DB_FILE  ID
+tirra-cli stat    DB_FILE
+tirra-cli decrypt DB_FILE
+tirra-cli encrypt PL_FILE 
 
 
 DB_FILE         : path to a tirra database
@@ -64,16 +64,15 @@ pub fn process(args: &Vec<String>, args_count: usize) {
     if cli_action == CliAction::Version {
         println!("tirra {}", version::VERSION);
     } else if cli_action == CliAction::Add {
-        if args_count != 5 {
+        if args_count != 4 {
             panic!("{}", USAGE_STR);
         }
 
         let db_path = String::from(&args[2]);
-        let pwd_file = String::from(&args[3]);
-        let timestamp = args[4].parse().unwrap();
+        let timestamp = args[3].parse().unwrap();
 
         //init db
-        let mut tirra_db = init_db(db_path, pwd_file);
+        let mut tirra_db = init_db_with_pwd(db_path);
 
         // Read the content of a file, and add an entry
         let mut tx_file = File::open("tirra-source.txt").unwrap();
@@ -98,16 +97,15 @@ pub fn process(args: &Vec<String>, args_count: usize) {
             }
         }
     } else if cli_action == CliAction::Delete {
-        if args_count != 5 {
+        if args_count != 4 {
             panic!("{}", USAGE_STR);
         }
 
         let db_path = String::from(&args[2]);
-        let pwd_file = String::from(&args[3]);
-        let id = args[4].parse().unwrap();
+        let id = args[3].parse().unwrap();
 
         //init db
-        let mut tirra_db = init_db(db_path, pwd_file);
+        let mut tirra_db = init_db_with_pwd(db_path);
 
         tirra_db.access_start().unwrap();
         let removed = tirra_db.api_remove_entry(id, true);
@@ -132,15 +130,17 @@ pub fn process(args: &Vec<String>, args_count: usize) {
         let mut tirra_db = init_db_with_pwd(db_path);
         print_stats(&mut tirra_db);
     } else if cli_action == CliAction::Decrypt {
-        if args_count != 4 {
+        if args_count != 3 {
             println!("{}", USAGE_STR);
             return;
         }
 
         let db_path = String::from(&args[2]);
-        let pwd_file = String::from(&args[3]);
 
-        let pwd = read_pwd_file(&pwd_file);
+        // Get Password
+        let password: String = ask_for_pwd();
+        let pwd = password.as_bytes();
+
         let plain_path = format!("{}.tirra", db_path);
         let mut crypto = TirraCrypto::new(&db_path, &plain_path, &pwd);
 
@@ -153,16 +153,18 @@ pub fn process(args: &Vec<String>, args_count: usize) {
             }
         }
     } else if cli_action == CliAction::Encrypt {
-        if args_count != 4 {
+        if args_count != 3 {
             println!("{}", USAGE_STR);
             return;
         }
 
         let clear_path = String::from(&args[2]);
         let enc_path = format!("{}.tirrage", clear_path);
-        let pwd_file = String::from(&args[3]);
+        
+        // Get Password
+        let password: String = ask_for_pwd();
+        let pwd = password.as_bytes();
 
-        let pwd = read_pwd_file(&pwd_file);
         let crypto = TirraCrypto::new(&enc_path, &clear_path, &pwd);
 
         match crypto.encrypt_file(&pwd) {
@@ -174,15 +176,17 @@ pub fn process(args: &Vec<String>, args_count: usize) {
             }
         }
     } else if cli_action == CliAction::Test {
-        if args_count != 4 {
+        if args_count != 3 {
             println!("{}", USAGE_STR);
             return;
         }
 
         let db_path = String::from(&args[2]);
-        let pwd_file = String::from(&args[3]);
+        
+        // Get Password
+        let password: String = ask_for_pwd();
+        let pwd = password.as_bytes();
 
-        let pwd = read_pwd_file(&pwd_file);
         let mut tirra_db = TirraDb::with_crypto(&db_path, &pwd);
         // test db ops
         let access = tirra_db.try_access();
@@ -193,17 +197,6 @@ pub fn process(args: &Vec<String>, args_count: usize) {
 /**
  * Init Db Access
  */
-fn init_db(db_path: String, pwd_path: String) -> TirraDb {
-    // Get Password
-    let pwd = read_pwd_file(&pwd_path);
-    //Init Tirra Db
-    let tirra_db = TirraDb::with_crypto(&db_path, &pwd);
-    //check if the database if found.
-    //if TirraDb::db_exists(&db_path) == false {
-    //    panic!("database file doesn't exist");
-    //}
-    return tirra_db;
-}
 
 fn init_db_with_pwd(db_path: String) -> TirraDb {
     // Get Password
@@ -215,13 +208,6 @@ fn init_db_with_pwd(db_path: String) -> TirraDb {
     return tirra_db;
 }
 
-fn read_pwd_file(pwd_file: &str) -> Vec<u8> {
-    let mut tx_file = File::open(pwd_file).unwrap();
-    let mut content_pwd = Vec::new();
-    tx_file.read_to_end(&mut content_pwd).unwrap();
-
-    content_pwd
-}
 
 fn print_stats(db: &mut TirraDb) {
     db.access_start().unwrap();
