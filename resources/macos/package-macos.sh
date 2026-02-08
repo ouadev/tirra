4478c5a90a7bfb8802bd0d8874754f630c8db403
ouadev <ouadev@users.noreply.github.com>
2026-02-08 05:27:29 +0100

    Tirra v1.1.0

--- resources/macos/package-macos.sh
+++ resources/macos/package-macos.sh
@@ -23,7 +23,7 @@ if [ ! -d "$SDK_DIRECTORY" ]; then
    fi
 fi
 
-WLVERSION="1.1.0-7"
+WLVERSION="1.1.0"
 DESTINATION="packages/macos-release"
 SOURCE_DIR="target/release"
 RESOURCES="resources"
--- src/common/version.rs
+++ src/common/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "v1.1.0-rc7";
+pub const VERSION: &str = "v1.1.0";
