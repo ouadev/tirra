0d3094973fe9d59960b7c42d1a27f822661f659d
ouadev <ouadev@users.noreply.github.com>
2026-04-14 23:00:00 +0200

    Tirra v2.0.0-rc2

--- resources/macos/package-macos.sh
+++ resources/macos/package-macos.sh
@@ -23,7 +23,7 @@ if [ ! -d "$SDK_DIRECTORY" ]; then
    fi
 fi
 
-WLVERSION="2.0.0-rc1"
+WLVERSION="2.0.0-rc2"
 DESTINATION="packages/macos-release"
 SOURCE_DIR="target/release"
 RESOURCES="resources"
